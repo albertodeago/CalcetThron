615c7e0b61a7c3475f66a5e202ee0d179f229751
Alberto De Agostini <albertodeagostini88@gmail.com>
2019-06-03 22:42:56 +0200

    change Ranking page accordingly

--- src/pages/Ranking.vue
+++ src/pages/Ranking.vue
@@ -1,51 +1,68 @@
 <template>
   <div class="q-pa-md">
     <q-list bordered padding>
-        <template v-for="(enrichedUser, index) in enrichedUsers">
-            <q-item :key="enrichedUser.id" clickable @click="openUser(enrichedUser)">
-                <q-item-section top avatar>
-                    <q-avatar>
-                        <q-img :src="enrichedUser.avatar" :ratio="1" />
-                    </q-avatar>
-                </q-item-section>
+        <template  v-if="!isLoading">
+            <template v-for="(enrichedUser, index) in sortedRankings">
+                <q-item :key="enrichedUser.id" clickable @click="openUser(enrichedUser)">
+                    <q-item-section top avatar>
+                        <q-avatar>
+                            <q-img :src="enrichedUser.avatar" :ratio="1" />
+                        </q-avatar>
+                    </q-item-section>
 
-                <q-item-section>
-                    <q-item-label>{{ enrichedUser.nickname }}</q-item-label>
-                    <q-item-label caption>Played games: {{ enrichedUser.played }}</q-item-label>
-                    <q-item-label caption>Goals done: {{ enrichedUser.goalDone }}</q-item-label>
-                    <q-item-label caption>Goals received: {{ enrichedUser.goalReceived }}</q-item-label>
-                    <q-item-label caption>Autogoals done: {{ enrichedUser.autogoalDone }}</q-item-label>
-                </q-item-section>
+                    <q-item-section>
+                        <q-item-label>{{ enrichedUser.nickname }}</q-item-label>
+                        <q-item-label caption>Played games: {{ enrichedUser.played }}</q-item-label>
+                        <q-item-label caption>Goals done: {{ enrichedUser.goalDone }}</q-item-label>
+                        <q-item-label caption>Goals received: {{ enrichedUser.goalReceived }}</q-item-label>
+                        <q-item-label caption>Autogoals done: {{ enrichedUser.autogoalDone }}</q-item-label>
+                    </q-item-section>
 
-                <q-item-section side top>
-                    <q-badge :label="'winrate: ' + enrichedUser.winRate" />
-                </q-item-section>
+                    <q-item-section side top>
+                        <q-badge :label="'winrate: ' + enrichedUser.winRate" />
+                    </q-item-section>
 
-            </q-item>
-            <q-separator spaced inset="item" v-if="index !== enrichedUsers.length-1" :key="enrichedUser.id + '_separator'" />
+                </q-item>
+                <q-separator spaced inset="item" v-if="index !== enrichedUsers.length-1" :key="enrichedUser.id + '_separator'" />
+            </template>
         </template>
     </q-list>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex"
-import { RankingBuilder } from "../models"
-import EnrichedUsersMixin from "../mixins/EnrichedUsers"
+import { mapGetters, mapActions } from "vuex"
 
 export default {
     data() {
         return {
-            enrichedUsers: []
+            enrichedUsers: [],
+            sortBy: "winRate"
         }
     },
 
-    mixins: [EnrichedUsersMixin],
+    computed: {
+        ...mapGetters("Global", ["isLoading"]),
+        ...mapGetters("Rankings", ["allRankingsArray"]),
+
+        sortedRankings() {
+            return this.allRankingsArray.slice().sort((a, b) => parseInt(b[this.sortBy].slice(0, -1)) - parseInt(a[this.sortBy].slice(0, -1)))
+        }
+    },
 
     methods: {
+        ...mapActions('Global', ['setLoading' ]),
+        ...mapActions('Rankings', ['getRankings']),
+
         openUser(enrichedUser) {
             this.$router.push(`user/${enrichedUser.id}`)
         }
+    },
+
+    async created() {
+        this.setLoading(true)
+        await this.getRankings()
+        this.setLoading(false)
     }
 }
 </script>
