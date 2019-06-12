85ffe4d3d8ee055354e220b9f14b52dca53db112
Alberto De Agostini <albertodeagostini88@gmail.com>
2019-06-12 21:38:30 +0200

    BUMP VERSION

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -8,7 +8,7 @@
                     <q-item-section>
                         <q-item-label>Application version</q-item-label>
                         <q-item-label caption>
-                            0.2.0-rc2
+                            0.2.0-rc3
                         </q-item-label>
                     </q-item-section>
                 </q-item>
