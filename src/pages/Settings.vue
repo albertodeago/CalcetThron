869da4e485df902fc71e689b29b969ee4756181f
Alberto De Agostini <albertodeagostini88@gmail.com>
2019-07-25 22:16:24 +0200

    version bump

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -8,7 +8,7 @@
                     <q-item-section>
                         <q-item-label>Application version</q-item-label>
                         <q-item-label caption>
-                            2.0.0-rc3
+                            2.0.0-rc4
                         </q-item-label>
                     </q-item-section>
                 </q-item>
