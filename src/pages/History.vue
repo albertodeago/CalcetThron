dd06eed4f0d487e54ba811eefbfe0f35ad5e40dd
Alberto De Agostini <albertodeagostini88@gmail.com>
2019-12-14 18:28:05 +0100

    wip, not sure it's the right way tho

--- src/pages/History.vue
+++ src/pages/History.vue
@@ -1,20 +1,22 @@
 <template>
     <div class="">
-        <template  v-if="!isLoading">
-            <div v-for="(users, day) in getHistory" :key="day">
+        <template v-if="!isLoading">
+            <div style="height: 100vh; width100vw; position: relative">
+                <canvas ref="canvas" id="canvas"></canvas>
+            </div>
+            <!-- <div v-for="(users, day) in getHistory" :key="day">
                 <h3>{{day}}</h3>
                 <div v-for="(rank, id) in users" :key="id">
                     {{allUsers[id].nickname}} - {{rank.ELO}}
                 </div>
-            </div>
-            <div>------------------------------------------</div>
-            <canvas ref="canvas"></canvas>
+            </div> -->
         </template>
     </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex"
+import Utils from "../Utils"
 import Chart from "chart.js"
 
 export default {
@@ -26,77 +28,152 @@ export default {
 
     computed: {
         ...mapGetters("Global", ["isLoading"]),
-        ...mapGetters('User', ['allUsers']),
-        ...mapGetters("History", ["getHistory"]),
+        ...mapGetters('User', ['allUsers', 'allUsersArray']),
+        ...mapGetters("History", ["getHistory", "getActiveUsers"]),
+
+        historyReady() {
+            return this.getHistory !== null
+        }
+    },
+
+    watch: {
+        historyReady(val) {
+            if (val) 
+                // this.createHistoryGraph()
+                this.createHistogram()
+        }
     },
 
     methods: {
-        ...mapActions('Global', ['setLoading' ])
-    },
+        ...mapActions('Global', ['setLoading' ]),
 
-    mounted() {
-        setTimeout(() => {
-            console.log("creating graph")
+        createHistogram() {
+            const history = this.getHistory
+            const days = Object.keys(history)
+            console.log(history)
+            
+            const getRandomColor = () => {
+                var letters = '0123456789ABCDEF'.split('');
+                var color = '#';
+                for (var i = 0; i < 6; i++ ) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
+                return color;
+            }
+
+            const datasets = [{
+                label: "Players",
+                fill: false,
+                backgroundColor: "#f00",
+                borderColor: "#f00",
+                borderWidth: 1,
+                data: this.getActiveUsers.map(user => history[days[days.length -1]][user.id].ELO)
+            }]
+
             var ctx = this.$refs.canvas;
             var myChart = new Chart(ctx, {
-                type: 'line',
+                type: 'horizontalBar',
                 data: {
-                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                    datasets: [{
-                        label: 'My First dataset',
-                        borderColor: "#ff0000",
-                        backgroundColor: "#ff0000",
-                        fill: false,
-                        data: [
-                            1,2,3,4,5,6,7
-                        ],
-                        yAxisID: 'y-axis-1',
-                    }, {
-                        label: 'My Second dataset',
-                        borderColor: "#0000ff",
-                        backgroundColor: "#0000ff",
-                        fill: false,
-                        data: [
-                            0,2,5,3,1,9,5
-                        ],
-                        yAxisID: 'y-axis-2'
-                    }]
+                    labels: this.getActiveUsers.map(u => u.nickname),
+                    datasets
                 },
                 options: {
                     responsive: true,
+                    maintainAspectRatio: false,
                     hoverMode: 'index',
                     stacked: false,
                     title: {
-                        display: true,
-                        text: 'Chart.js Line Chart - Multi Axis'
+                        display: false,
+                        text: 'Do we set a name?'
                     },
-                    scales: {
-                        yAxes: [{
-                            type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-                            display: true,
-                            position: 'left',
-                            id: 'y-axis-1',
-                        }, {
-                            type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-                            display: true,
-                            position: 'right',
-                            id: 'y-axis-2',
-
-                            // grid line settings
-                            gridLines: {
-                                drawOnChartArea: false, // only want the grid lines for one axis to show up
-                            },
-                        }],
+                    legend: {
+                        display: false
                     }
+                    // scales: {
+                    //     xAxes: [{
+                    //         type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                    //         display: true,
+                    //         position: 'left',
+                    //         id: 'x-axis',
+                    //     }],
+                    // }
                 }
             });
-        }, 2500)
+        }
+
+        // createHistoryGraph() {
+        //     const history = this.getHistory
+        //     const days = Object.keys(history)
+        //     console.log(history)
+            
+        //     const getRandomColor = () => {
+        //         var letters = '0123456789ABCDEF'.split('');
+        //         var color = '#';
+        //         for (var i = 0; i < 6; i++ ) {
+        //             color += letters[Math.floor(Math.random() * 16)];
+        //         }
+        //         return color;
+        //     }
+
+        //     const randomUsers = Utils.shuffle(this.getActiveUsers.slice())
+        //     const datasets = randomUsers.slice(0,3).map(user => {
+        //         const userHistory = days.map(day => history[day][user.id].ELO)
+        //         const userColor = getRandomColor()
+        //         return {
+        //             label: user.nickname,
+        //             fill: false,
+        //             backgroundColor: userColor,
+        //             borderColor: userColor,
+        //             data: userHistory
+        //         }
+        //     })
+
+        //     var ctx = this.$refs.canvas;
+        //     var myChart = new Chart(ctx, {
+        //         type: 'line',
+        //         data: {
+        //             labels: days,
+        //             datasets
+        //         },
+        //         options: {
+        //             responsive: false,
+        //             maintainAspectRatio: false,
+        //             hoverMode: 'index',
+        //             stacked: false,
+        //             title: {
+        //                 display: false,
+        //                 text: 'Do we set a name?'
+        //             },
+        //             scales: {
+        //                 yAxes: [{
+        //                     type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+        //                     display: true,
+        //                     position: 'left',
+        //                     id: 'y-axis-1',
+        //                 }],
+        //             }
+        //         }
+        //     });
+        // }
+    },
+
+    mounted() {
+        if (this.historyReady) 
+            // this.createHistoryGraph()
+            this.createHistogram()
     }
 }
 </script>
 
 <style lang="stylus" scoped>
 
+#canvas {
+    position absolute;
+    left:0;
+    right: 0;
+    bottom:0;
+    top: 0;
+}
 </style>
 
 
--- src/store/history/index.js
+++ src/store/history/index.js
@@ -1,4 +1,5 @@
 import Config from "src/Config"
+import Utils from "src/Utils"
 
 
 export default {
@@ -6,10 +7,12 @@ export default {
     state: {
         collectionName: null,
         history: null,
+        activeUsers: []
     },
 
     getters: {
-        getHistory: (state) => state.history
+        getHistory: (state) => state.history,
+        getActiveUsers: (state) => state.activeUsers
     },
     mutations: {
 
@@ -23,23 +26,99 @@ export default {
 
         setHistory(state, history) {
             state.history = history
+        },
+
+        setActiveUsers(state, users) {
+            state.activeUsers = users
         }
 
     },
     actions: {
 
         async getHistory({ state, commit, rootState }) {
-            if (!rootState.Seasons.selectedSeason || !state.collectionName)
+            if (!rootState.Seasons.selectedSeason || !state.collectionName || !rootState.User.allUsersArray.length) {
+                console.warn("season or users not ready to build the history!")
                 return
+            }
 
             const db = firebase.firestore()
             const historyCollection = db.collection(state.collectionName)
 
             const historySnapshot = await historyCollection.get()
-            const days = {}
-            historySnapshot.forEach(snap => days[snap.id] = snap.data())
+            const daysHistory = {}
+            historySnapshot.forEach(snap => daysHistory[snap.id] = snap.data())
 
-            commit("setHistory", days)
+
+            // prepare history by skipping empty days and filling missing ranks when a player don't play or has never played
+            const days = Object.keys(daysHistory) // days are in pattern yyyy_mm_dd
+            const firstDayWithDash = days[0].split("_").join("-")
+            const lastDayWithDash = days[days.length -1].split("_").join("-")
+            const date = new Date(firstDayWithDash)
+            const lastDate  = new Date(lastDayWithDash)
+
+            const formatDate = (date) => {
+                const year = date.getFullYear();
+                const month = date.getMonth() + 1;
+                const day = (date.getDate() + "").padStart(2, "0");
+                return `${year}_${month}_${day}`; // e.g. 2019_12_07
+            }
+
+            const fullHistory = {}
+            const initialRank = {
+                id: null,
+                nickname: "",
+                played: 0,
+                won: 0,
+                lost: 0,
+                winRate: "0%",
+                playedGoalkeeper: 0,
+                wonGoalkeeper: 0,
+                lostGoalkeeper: 0,
+                winRateGoalkeeper: "0%",
+                playedStriker: 0,
+                wonStriker: 0,
+                lostStriker: 0,
+                winRateStriker: "0%",
+                goalDone: 0,
+                goalDoneAsStriker: 0,
+                goalDoneAsGoalkeeper: 0,
+                goalReceived: 0,
+                autogoalDone: 0,
+                ELO: 1000
+            }
+            let previousHistoryday = {}
+
+            // find users that have played this season
+            const activeIds = []
+            days.forEach(day => {
+                const ids = Object.keys(daysHistory[day])
+                ids.forEach(id => Utils.pushUnique(activeIds, id))
+            })
+            const activeUsers = rootState.User.allUsersArray.filter(u => activeIds.indexOf(u.id) > -1)
+            console.log("active players for this season", activeUsers.map(u => u.nickname))
+            activeUsers.forEach(user => previousHistoryday[user.id] = Object.assign({}, initialRank, {id: user.id, nickname: user.nickname}))
+            console.log("starting from: ", previousHistoryday)
+
+            while (date.getTime() <= lastDate.getTime()) {
+                // if the history don't have this day, we skip the day
+                if (daysHistory[formatDate(date)]) {       
+                    fullHistory[formatDate(date)] = {}
+
+                    // for each user we set the rank of the day either taking it form DB or from taking the previous value
+                    // if a player never played he will have always the same object as initialRank
+                    activeUsers.forEach(user => {
+                        if (daysHistory[formatDate(date)][user.id]) {
+                            fullHistory[formatDate(date)][user.id] = daysHistory[formatDate(date)][user.id]
+                        } else {
+                            fullHistory[formatDate(date)][user.id] = previousHistoryday[user.id]
+                        }
+                    })
+                }
+                date.setDate(date.getDate() + 1) // increment 1 day
+            }
+
+            commit("setActiveUsers", activeUsers)
+            commit("setHistory", fullHistory)
         },
 
         selectSeason({ commit }, season) {
