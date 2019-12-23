d710aea56686a31457251ddf71560bc36ecfa86b
Alberto De Agostini <albertodeagostini88@gmail.com>
2019-12-23 15:05:48 +0100

    add firebase config sample

---
+++ scripts/firebase-sample.js
@@ -0,0 +1,13 @@
+// uncomment this lines and fill with your config data and rename this file as "firebase.js"
+// export default async() => {
+//     // Initialize Firebase
+//     var config = {
+//         apiKey: "your apikey",
+//         authDomain: "your authDomain",
+//         databaseURL: "your databaseURL",
+//         projectId: "your projectId",
+//         storageBucket: "your storageBucket",
+//         messagingSenderId: "your senderId"
+//     };
+//     firebase.initializeApp(config);
+// }
